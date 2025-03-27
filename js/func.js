7b302f6352b6e5081f8dee5e8a33e57dcf6c969f
Xkalivur <Xkalivur001@outlook.com>
2025-03-27 07:44:49 +0000

    https://www.geeksforgeeks.org/pass-javascript-variables-to-python-in-flask/ look this website to continue

--- js/func.js
+++
@@ -1,4 +0,0 @@
-function addcode() {
-  let host_name = document.getElementById("host_name").value;
-  localStorage.setItem(JSON.stringify(host_name),undefined)
-}
--- python/main.py
+++ python/main.py
@@ -1,1 +1,9 @@
+from localStoragePy import localStoragePy
+import json
 
+localStorage = localStoragePy('codes', 'json')
+
+with open("code.json","w") as f:
+    code = localStorage.getItem()
+    print(code)
+    json.dump(code)
