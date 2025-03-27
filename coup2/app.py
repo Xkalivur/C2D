b79ea9efebf3dc9c0b1db42a5b0e0528c7329dd7
122727 <122727@combertonvc.org>
2025-03-27 13:02:08 +0000

    fixed css file to make sure it works

--- coup2/app.py
+++ coup2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request, jsonify
+from flask import Flask,render_template, request
 
 app = Flask(__name__,template_folder="templates")
 
@@ -8,10 +8,10 @@ def hello():
 
 @app.route('/process', methods=['POST'])
 def process():
-    data = request.get_json() # retrieve the data sent from JavaScript
+    data = request.form.get('data')
     # process the data using Python code
-    result = data['value'] * 2
-    return jsonify(result=result) # return the result to JavaScript
+    result = data.upper()
+    return result
 
 if __name__ == '__main__':
     app.run(debug=True)
