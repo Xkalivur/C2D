5b0e9bdf47fe7276cdd2b342a3c400b415c24097
122727 <122727@combertonvc.org>
2025-03-31 14:54:15 +0100

    Delete python directory

--- python/main.py
+++
@@ -1,9 +0,0 @@
-from localStoragePy import localStoragePy
-import json
-
-localStorage = localStoragePy('codes', 'json')
-
-with open("code.json","w") as f:
-    code = localStorage.getItem()
-    print(code)
-    json.dump(code)
