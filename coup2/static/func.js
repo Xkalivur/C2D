a6c481f66f38646449a4017b79159cf4305b7517
122727 <122727@combertonvc.org>
2025-03-31 14:54:02 +0100

    Delete coup2 directory

--- coup2/static/func.js
+++
@@ -1,9 +0,0 @@
-function addcode() {
-  var value = document.getElementById('host_name').value;
-            $.ajax({
-                url: '/process',
-                type: 'POST',
-                contentType: 'application/json',
-                data: JSON.stringify({ "value": value })
-            });
-}
