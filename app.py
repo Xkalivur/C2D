db7214f3493dda1386b5e4077dbeeed8e4a84958
Xkalivur <Xkalivur001@outlook.com>
2025-04-05 10:51:49 +0000

    this doesn't work with flask

--- app.py
+++ app.py
@@ -4,32 +4,32 @@ import random
 
 codelist="ABCDEFGHIJKLMNOPQRSTUVWXYZ012345678901234567890123456789"
 
-def start():
-    app = Flask(__name__,template_folder="templates")
 
-    @app.route("/")
-    def hello():
-        return render_template('start.html')
+app = Flask(__name__,template_folder="templates")
 
-    @app.route('/create')
-    def create():
-        return render_template('create.html')
+@app.route("/")
+def hello():
+    return render_template('start.html')
 
-    @app.route('/process', methods=['POST'])
-    def process():
-        data = request.form.get('data')
-        result = data
-        code = ''.join(random.choice(codelist) for _ in range(6))
-        with open('data.json', 'r') as file: 
-            data = json.load(file)
-        if result in data.keys():
-            return result
-        if result == "":
-            return "Empty string cannot be used"
-        data[result] = code
-        with open('data.json', 'w') as file: 
-            json.dump(data, file, indent=6)
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    data = request.form.get('data')
+    result = data
+    code = ''.join(random.choice(codelist) for _ in range(6))
+    with open('data.json', 'r') as file: 
+        data = json.load(file)
+    if result in data.keys():
         return result
-        
-    if __name__ == '__main__':
-        app.run(debug=True)
+    if result == "":
+        return "Empty string cannot be used"
+    data[result] = code
+    with open('data.json', 'w') as file: 
+        json.dump(data, file, indent=6)
+    return result
+    
+if __name__ == '__main__':
+    app.run(debug=True)
